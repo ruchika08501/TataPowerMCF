a708900bbd96657c492a006c2857c96635ba1088
servproSushant <sushant.lawate@servpro.in>
2018-02-05 16:47:29 +0530

    my account services logo

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,75 +1,71 @@
 sap.ui.define([
 	"sap/ui/demo/nav/controller/BaseController"
-], function (BaseController) {
+], function(BaseController) {
 	"use strict";
 
 	return BaseController.extend("sap.ui.demo.nav.controller.Home", {
 
-		onDisplayNotFound : function (oEvent) {
+		onDisplayNotFound: function(oEvent) {
 			// display the "notFound" target without changing the hash
 			this.getRouter().getTargets().display("notFound", {
-				fromTarget : "home"
+				fromTarget: "home"
 			});
 		},
 
-	   onInit: function() {
-	  	     	$("#__panel0").hide();
-			}
-,
+		onInit: function() {
+
+			$("#__panel0").hide();
+		},
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
 		 * @memberOf view.MyAccount
 		 */
 		onBeforeRendering: function() {
-		  	$("#__panel0").hide();
-			},
+
+			$("#__panel0").hide();
+		},
 
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
 		 * @memberOf view.MyAccount
 		 */
-			onAfterRendering: function() {
-		  	$("#__panel0").hide();
-			},
+		onAfterRendering: function() {
+			$("#__panel0").hide();
+
+		},
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
 		 * @memberOf view.MyAccount
 		 */
 		onExit: function() {
-		  	$("#__panel0").hide();
-			},
-        goDown : function (oEvent) {
-        	
-        		// var oFrame1 = this.getView().byId("toggletab");
-        		// alert(oFrame1);
-        	$("#__panel0").slideToggle("slow");
-        },
+			$("#__panel0").hide();
 
-		onNavToMyAccount : function (oEvent){
-			
+		},
+
+		onNavToMyAccount: function(oEvent) {
+
 			this.getRouter().navTo("MyAccount");
-		
+
 		},
-			onNavToQuickBill : function (oEvent){
-			
+		onNavToQuickBill: function(oEvent) {
+
 			this.getRouter().navTo("PaymentHistory");
-		
+
 		},
-			onNavToPaybill : function (oEvent){
-			
+		onNavToPaybill: function(oEvent) {
+
 			this.getRouter().navTo("QuickPayment");
-		
+
 		},
-onHomePress : function (oEvent){
-		
+		onHomePress: function(oEvent) {
+
 			this.getRouter().navTo("home");
-		
-		}
-,
-		onNavToEmployeeOverview : function (oEvent) {
+
+		},
+		onNavToEmployeeOverview: function(oEvent) {
 			this.getRouter().navTo("employeeOverview");
 		}
 
